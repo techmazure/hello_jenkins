ecf3b372edd31ab3f23573234fa452e824c6f24f
niranjan <thummalaniranjan8@gmail.com>
2020-05-10 14:38:13 +0530

    modified code

--- src/main/java/com/techm/Hello/HelloApplication.java
+++ src/main/java/com/techm/Hello/HelloApplication.java
@@ -2,12 +2,18 @@ package com.techm.Hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloApplication {
+public class HelloApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
-
+@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	// TODO Auto-generated method stub
+	return builder.sources(HelloApplication.class);
+}
 }
